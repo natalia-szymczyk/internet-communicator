d9bf898d60cc92c0bb79bc535688769e82586944
natalia-szymczyk <natalia@DESKTOP-VIDDVSV.localdomain>
2022-01-24 12:21:44 +0100

    server -clean code, ale na localhoscie

--- server/serverTCPmultithread.c
+++ server/serverTCPmultithread.c
@@ -4,70 +4,57 @@
 #include<netinet/in.h>
 #include<string.h>
 #include <arpa/inet.h>
-#include <fcntl.h> 
-#include <unistd.h>
+#include <fcntl.h>
+#include <unistd.h> 
 #include<pthread.h>
 
 // gcc -g -Wall -pthread yourcode.c -lpthread -o yourprogram
 
-#define PORT 8887
+#define PORT 8828
 #define MAX_CLIENTS 100
 #define MSG_SIZE 2048
 
 char client_message[MSG_SIZE];
-char user_list[30][50];
 unsigned int client_number = 0;
-char buffer[MSG_SIZE];
 static int uid = 10;
 
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-
-typedef struct{
+typedef struct Client{
 	struct sockaddr_storage address;
 	int sockfd;
 	int uid;
-	char name[32];
-} client_t;
+	char login[32];
+} Client;
 
-client_t *clients[MAX_CLIENTS];
+Client *clients[MAX_CLIENTS];
 
-void print_users(){
+
+// send message to user with defined login
+void send_message(char *s, char* login){
 	pthread_mutex_lock(&clients_mutex);
 
-	for(int i=0; i < MAX_CLIENTS; ++i){
+	for(int i = 0; i < MAX_CLIENTS; ++i){
 		if(clients[i]){
-			printf("\n %i --> %s \n", i, clients[i]->name);
-			}
-		}
-
-	pthread_mutex_unlock(&clients_mutex);
-}
-
-
-void send_message(char *s, char* name){
-	pthread_mutex_lock(&clients_mutex);
-
-	for(int i=0; i<MAX_CLIENTS; ++i){
-		if(clients[i]){
-			if(strcmp(clients[i]->name, name) == 0 ){
+			if(strcmp(clients[i]->login, login) == 0 ){
         if(send(clients[i]->sockfd, s, strlen(s), 0) < 0){
-          perror("ERROR: write to descriptor failed");
+          perror("ERROR: Sending failed");
           break;
         }
-      }
+      }			
 		}
 	}
 
 	pthread_mutex_unlock(&clients_mutex);
 }
 
-void queue_add(client_t *cl){
+// add client to the list of clients
+void add_client(Client *client){
 	pthread_mutex_lock(&clients_mutex);
 
 	for(int i = 0; i < MAX_CLIENTS; ++i){
 		if(!clients[i]){
-			clients[i] = cl;
+			clients[i] = client;
 			break;
 		}
 	}
@@ -75,10 +62,11 @@ void queue_add(client_t *cl){
 	pthread_mutex_unlock(&clients_mutex);
 }
 
-void queue_remove(int uid){
+// remove client from the list of clients
+void remove_client(int uid){
 	pthread_mutex_lock(&clients_mutex);
 
-	for(int i=0; i < MAX_CLIENTS; ++i){
+	for(int i = 0; i < MAX_CLIENTS; ++i){
 		if(clients[i]){
 			if(clients[i]->uid == uid){
 				clients[i] = NULL;
@@ -90,33 +78,47 @@ void queue_remove(int uid){
 	pthread_mutex_unlock(&clients_mutex);
 }
 
+// print all active users
+void print_users(){
+	pthread_mutex_lock(&clients_mutex);
 
-void * socketThread(void *arg){
+  printf("\n\nACTIVE USERS:\n");
+
+	for(int i = 0; i < MAX_CLIENTS; ++i){
+		if(clients[i]){
+			printf("\n %i --> %s \n", i, clients[i]->login);
+			}
+		}
+
+	pthread_mutex_unlock(&clients_mutex);
+}
+
+
+void *socketThread(void *arg){
   printf("%d. THREAD \n", client_number);
-  client_t *cli = (client_t *)arg;
 
+  Client *client = (Client *)arg;
   client_number++;
 
   int n;
 
-  for(;;){
-    n = recv(cli->sockfd , client_message , 2000 , 0);
-    printf("RECV: %s\n",client_message);
-        if(n < 1){
-            break;
-        }
+  while(1){
+    n = recv(client->sockfd , client_message , 2000 , 0);
+
+    if(n < 1){
+        break;
+    }
+
+    // printf("\nRECV: %s\n",client_message);
 
     char *token;
     char msg[MSG_SIZE];
     char delimiter[] = "++"; 
     char receiver[64];
 
-    char *message = malloc(MSG_SIZE);
-    strcpy(message,client_message);
+    token = strtok(client_message, delimiter); // key word
 
-    token = strtok(message, delimiter); // slowo klucz
-
-    printf(">> %s >> ", token);
+    printf("\n>> %s >> ", token);
 
     if (strcmp(token, "MSG") == 0){
       token = strtok(NULL, delimiter);
@@ -130,44 +132,50 @@ void * socketThread(void *arg){
       strcpy(receiver, token);
 
       token = strtok(NULL, delimiter);
-      printf("IS %s \n", token);
+      printf("IS %s ", token);
       strcat(msg, token);
       strcat(msg, "++");
-
       
-      printf("ALL: FROM %s TO %s IS %s ", cli->name, receiver, msg);
-
+      // printf("ALL: FROM %s TO %s IS %s ", client->login, receiver, msg);
 
       if(strlen(client_message) > 0){
 				send_message(msg, receiver);
       }
+    }    
 
-
-    }
     else if (strcmp(token, "LOGIN") == 0){
       token = strtok(NULL, delimiter);
-      strcpy(cli->name, token);
+      strcpy(client->login, token);
 
-      printf("**%s** LOGGED IN", cli->name);
+      printf("**%s** LOGGED IN", client->login);
 
       print_users();
     }
+
     else if (strcmp(token, "LOGOUT") == 0){
-      printf("**%s** LOGGED OUT \n", cli->name);
+      printf("**%s** LOGGED OUT \n\n", client->login);
       break;
+    }
+
+    else if (strcmp(token, "CHAT") == 0){
+      token = strtok(NULL, delimiter);
+      printf("%s WITH", token);
+
+      token = strtok(NULL, delimiter);
+      printf(" %s ", token);
     }
 
     printf("\n");
 
     sleep(1);
-    bzero(client_message, MSG_SIZE);
-
+    // bzero(client_message, MSG_SIZE);
+    memset(client_message, 0, MSG_SIZE);
     }
 
     printf("EXIT SOCKETTHREAD \n");
-    close(cli->sockfd);
-    queue_remove(cli->uid);
-    free(cli);
+    close(client->sockfd);
+    remove_client(client->uid);
+    free(client);
     client_number--;
 
     pthread_exit(NULL);
@@ -175,65 +183,63 @@ void * socketThread(void *arg){
 
 
 int main(){
-  int serverSocket, newSocket;
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
+  int server_socket, client_socket;
+  struct sockaddr_in server_addr;
+  struct sockaddr_storage server_storage;
   socklen_t addr_size;
 
+  pthread_t thread_id;
+
   //Create the socket. 
-  serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+  server_socket = socket(PF_INET, SOCK_STREAM, 0);
 
   // Configure settings of the server address struct
   // Address family = Internet 
-  serverAddr.sin_family = AF_INET;
+  server_addr.sin_family = AF_INET;
 
   //Set port number, using htons function to use proper byte order 
-  serverAddr.sin_port = htons(PORT);
+  server_addr.sin_port = htons(PORT);
 
   //Set IP address to localhost 
-  serverAddr.sin_addr.s_addr = inet_addr("172.27.86.40");
-
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
   //Set all bits of the padding field to 0 
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+  memset(server_addr.sin_zero, '\0', sizeof server_addr.sin_zero);
   
-  // active users
-  memset(user_list, 0, sizeof user_list);
-
   //Bind the address struct to the socket 
-  bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+  bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
 
   //Listen on the socket
-  if(listen(serverSocket,50)==0)
-    printf("Listening\n");
+  if(listen(server_socket,50)==0)
+    printf("Listening on port %i\n\n", PORT);
   else
-    printf("Error\n");
+    printf("ERROR: Listening failed\n");
 
-    pthread_t thread_id;
 
   while(1){
       //Accept call creates a new socket for the incoming connection
-      addr_size = sizeof serverStorage;
-      newSocket = accept(serverSocket, (struct sockaddr *) &serverStorage, &addr_size);
+      addr_size = sizeof server_storage;
+      client_socket = accept(server_socket, (struct sockaddr *) &server_storage, &addr_size);
 
       if((client_number + 1) == MAX_CLIENTS){
-        printf("Max clients reached. Rejected: ");
-        close(newSocket);
+        printf("Maximum number of clients! \n");
+        close(client_socket);
         continue;
       }
 
-      client_t *cli = (client_t *)malloc(sizeof(client_t));
-      cli->address = serverStorage;
-      cli->sockfd = newSocket;
-      cli->uid = uid++;
+      Client *client = (Client *)malloc(sizeof(Client));
+      client->address = server_storage;
+      client->sockfd = client_socket;
+      client->uid = uid++;
 
-      queue_add(cli);
+      add_client(client);
 
-      if( pthread_create(&thread_id, NULL, socketThread, (void*)cli) != 0 )
+      if( pthread_create(&thread_id, NULL, socketThread, (void*)client) != 0 )
           printf("Failed to create thread\n");
 
       pthread_detach(thread_id);
       pthread_join(thread_id,NULL);
   }
-  return 0;
+
+  return EXIT_SUCCESS;
 }
